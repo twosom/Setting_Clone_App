2f3c7a5061eee231971a12e71cbe023065704227
desirel <two_somang@icloud.com>
2021-12-08 23:44:28 +0900

    Initial commit

---
+++ Setting_Clone_App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Setting_Clone_App
+//
+//  Created by Desire L on 2021/12/08.
+//
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
